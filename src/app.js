9218e19d6e967614cff7a2646b6e3464af0014dc
dvriet <dvriet@ictmbo.nl>
2017-10-12 21:10:17 +0200

    changed server address of wandelappbackend in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import {hikingapp} from './hikingapp';
 
 // Server that stores route data
-const remoteserver = 'https://nodejs-mongo-persistent-wandelappbackend-v2.a3c1.starter-us-west-1.openshiftapps.com';
+// const remoteserver = 'https://nodejs-mongo-persistent-wandelappbackend-v2.a3c1.starter-us-west-1.openshiftapps.com';
+const remoteserver = 'http://nodejs-mongo-persistent-wandelappbackend-v4.a3c1.starter-us-west-1.openshiftapps.com/';
 
 document.addEventListener('DOMContentLoaded', () => {
     //Load the app with the REST server
