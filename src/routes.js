9c3720196e5ec3f7db662f72b84eeff32a3282c9
WEBhicham <allatrona@gmail.com>
2018-05-23 10:01:29 +0200

    testing works now with the second command of the text_installation.txt file

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,33 @@
-import * as $ from 'jquery';
-
+let jsdom = require("jsdom/lib/old-api");
 /**
  * Read json from remoteserver
  * @param remoteserver
  * @returns {Promise}
  */
 const getroutesjson = (remoteserver) => {
-    return new Promise((resolve, reject) => { //New promise for array
-        // let routesjson = [];
-        fetch(remoteserver)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(myJson) {
-                const routesjson = data.map((f) => {
-                    return {data: f};
-                });
-                resolve(routesjson);
-                console.log(myJson);
-            });
-    //     $.ajax({
-    //             type: "GET",
-    //             url: remoteserver,
-    //             dataType: "json"
-    //         })
-    //         .done((data) => {
-    //         console.log(data);
-    //                 const routesjson = data.map((f) => {
-    //                     return {data: f};
-    //                 });
-    //                 resolve(routesjson);
-    //             }
-    //         )
-    //         .fail((err) => reject(err));
-    });
+	return new Promise((resolve, reject) => { //New promise for array	
+		jsdom.env("", function(err, window) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			var $ = require("jquery")(window);
+			$.ajax({
+				type: "GET",
+				url: remoteserver,
+				dataType: "json"
+			})
+				.done((data) => {
+					console.log(data);
+					const routesjson = data.map((f) => {
+						return {data: f};
+					});
+					resolve(routesjson);
+				}
+				)
+				.fail((err) => reject(err));
+		});
+	});
 };
 
 /**
@@ -44,35 +38,35 @@ const getroutesjson = (remoteserver) => {
  */
 const posttextfile = (remoteserver = "", file = "") => {
 
-    return new Promise((resolve, reject) => { //New promise for array
-        const reader = new FileReader();
-        //Send contents file when read
-        reader.onloadend = (e) => {
-            const contents = e.target.result;
+	return new Promise((resolve, reject) => { //New promise for array
+		const reader = new FileReader();
+		//Send contents file when read
+		reader.onloadend = (e) => {
+			const contents = e.target.result;
 
-            const xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        const res = JSON.parse(xhr.response);
-                        console.log(res);
-                        if(res.error === true){
-                            reject(res.msg);
-                        } else {
-                            resolve();
-                        }
-                    } else {
-                        reject("Problem posting " + xhr.status);
-                    }
-                }
-            };
+			const xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						const res = JSON.parse(xhr.response);
+						console.log(res);
+						if(res.error === true){
+							reject(res.msg);
+						} else {
+							resolve();
+						}
+					} else {
+						reject("Problem posting " + xhr.status);
+					}
+				}
+			};
 
-            xhr.open("POST", remoteserver);
-            xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
-            xhr.send(contents);
-        };
-        reader.readAsText(file);
-    });
+			xhr.open("POST", remoteserver);
+			xhr.overrideMimeType("text/plain; charset=x-user-defined-binary");
+			xhr.send(contents);
+		};
+		reader.readAsText(file);
+	});
 };
 
 //expose ajax functions
