73561196f2bc0a465e91c689a8318f623599ff23
geertsoetens <geert.soetens@ictmbo.nl>
2018-05-23 09:41:29 +0200

    Added ServiceWorker

---
+++ src/sw.js
@@ -0,0 +1,19 @@
+// This is the Service Worker. It helps the app function in an offline enviroment.
+// Geert Soetens
+
+importScripts('cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('wandelapp').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '../index.html',
+                'app.js',
+                'hikingapp.js',
+                'map.js',
+                'routes.js'
+            ])
+        })
+    )
+})
