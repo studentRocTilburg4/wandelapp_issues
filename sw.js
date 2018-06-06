fffa3895b444080b67ba911317be65c7ae3117a7
geertsoetens <geert.soetens@ictmbo.nl>
2018-06-06 10:45:26 +0200

    test

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ var urlsToCache = [
     'sw.js',
     'https://nodejs-mongo-persistent-wandelappbackend-v4.a3c1.starter-us-west-1.openshiftapps.com/routes?cuid=test',
 ];
-self.addEventListener('install', function(event) {
+self.addEventListener('install',    function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -25,7 +25,25 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
-                   return response || fetch(event.request);
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request).then(
+                        function(response) {
+                            if(!response || response.status !== 200 || response.type !== "basic") {
+                                    return response;
+                            }
+
+                            // let responseToCache = response.clone();
+                            
+                            // caches.open(CACHE_NAME)
+                            //     .then(function(cache) {
+                            //         cache.put(event.request, responseToCache);
+                            //     });
+                            // return response;
+                        }
+                    )
                 }
             )
     );
