17b58d6d2410532f0bcde5e4b6a56fd1ee4cc995
geertsoetens <geert.soetens@ictmbo.nl>
2018-06-08 09:29:36 +0200

    sw update

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const urlsToCache = [
+var CACHE_NAME = 'wandelapp-offline-cache';
+var urlsToCache = [
     '/',
     'css/wandelapp.css',
     'src/app.js',
@@ -10,38 +11,40 @@ const urlsToCache = [
     'sw.js',
     'https://nodejs-mongo-persistent-wandelappbackend-v4.a3c1.starter-us-west-1.openshiftapps.com/routes?cuid=test',
 ];
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install',    function(event) {
     // Perform install steps
     event.waitUntil(
-        caches.open('sw-wandelapp-cache')
+        caches.open(CACHE_NAME)
             .then(function(cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
 });
-addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;     // if valid response is found in cache return it
-          } else {
-            return fetch(event.request)     //fetch from internet
-              .then(function(res) {
-                return caches.open('sw-wandelapp-cache')
-                  .then(function(cache) {
-                    cache.put(event.request.url, res.clone());    //save the response for future
-                    return res;   // return the fetched data
-                  })
-              })
-              .catch(function(err) {       // fallback mechanism
-                return caches.open('sw-wandelapp-cache')
-                  
-                console.log("Fallback mechanism activated... Error : ", err);
-              });
-          }
-        })
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request).then(
+                        function(response) {
+                            if(!response || response.status !== 200 || response.type !== "basic") {
+                                    return response;
+                            }
+
+                            let responseToCache = response.clone();
+                            
+                            caches.open(CACHE_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request, responseToCache);
+                                });
+                            return response;
+                        }
+                    )
+                }
+            )
     );
-  }); 
+});
