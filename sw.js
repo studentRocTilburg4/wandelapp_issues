c91fc105ba5a82f49ab1fa35d63368b004e15a7a
geertsoetens <geert.soetens@ictmbo.nl>
2018-06-08 10:19:18 +0200

    updated SW

--- sw.js
+++ sw.js
@@ -22,10 +22,24 @@ self.addEventListener('install', function(event) {
     );
 });
 self.addEventListener('fetch', function(event) {
+    console.log('Handling fetch event for', event.request.url);
+
     event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                console.log(response);
-            })
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+            console.log('No response found in cache. About to fetch from network...');
+
+            return fetch(event.request).then(function(response) {
+                console.log('Response from network is:', response);
+
+                return response
+            }).catch(function(error) {
+                console.error('Fetching failed:', error)
+            });
+        })
     );
 });
